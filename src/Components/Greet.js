a77790ad32355e7a554aa6c5e170a611f6d8060e
Vaishnav13 <vaishnavsalunkhe20@gmail.com>
2024-01-29 17:38:51 +0530

    sujit is king once again

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -5,5 +5,7 @@ import React from 'react'
 // }
 
 const Greet = () => <h1>Hello Vaishnav</h1>
+const  sujit = () => <h1>sujit is king</h1>
+const  sujit2 = () => <h1>sujit is king</h1>
 
 export default Greet
