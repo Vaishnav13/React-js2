183229fd2b07bddf2fe93f19666b24b4c48cfd37
Vaishnav13 <vaishnavsalunkhe20@gmail.com>
2024-01-31 19:35:29 +0530

    test

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -6,6 +6,7 @@ const Greet = () => <h1>Hello Vaishnav</h1>
 const  sujit = () => <h1>sujit is king</h1>
 const  sujit2 = () => <h1>sujit is king</h1>
 const puls =  () => <h1>Hello Vaishnav</h1>
+const puls2 =  () => <h1>Hello Vaishnav</h1>
 
 
 export default Greet
